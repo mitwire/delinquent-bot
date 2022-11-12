8adec6329368c0e7ad2ea83e8a688ce8e6b417c8
Mitwire <mitwire@gmail.com>
2022-11-13 09:51:12 +1100

    upd

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  client.user.setActivity(`cats`, { type: "WATCHING" })
+
 });
 
 client.on('interactionCreate', async interaction => {
