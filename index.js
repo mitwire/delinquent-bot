8ab864a081c8cbaa6a1e349132e2dcc5b7f165a5
Mitwire <mitwire@gmail.com>
2022-11-13 09:45:44 +1100

    status

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const { REST, Routes } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const TOKEN = "MTA0MDc5OTA0ODMwMDUwNzIzOA.GZndv_.C9u50r5xVEauau8vfNWzR65OFSjsnklBgnl2FA";
 const CLIENT_ID = "1040799048300507238"
+const { ActivityType } = require('discord.js')
+module.exports = {
+  name: 'ready',
+  once: true,
+  async execute(client) {
+    const options = [
+      {
+        type: ActivityType.Watching,
+        text: "cats",
+        status: "online"
+      }
+    ]
+  },
+};
+
 
 const commands = [
     {
