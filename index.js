649c66e517aeb7051042f21085d39d791935a77e
Mitwire <mitwire@gmail.com>
2022-11-12 21:30:08 +1100

    update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('interactionCreate', async interaction => {
 */
   if (interaction.commandName === 'ping') {
     const pingvalue = client.ws.ping;
-    const description = 'Latency: ' + pingvalue + ' ms' + ' 🏓'
+    const description = 'Latency: ' + pingvalue + ' ms' 
     const exampleEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle('🏓 Pong!')
