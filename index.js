fe682f85dd5df7599ced61e34bec825c72da2602
Mitwire <mitwire@gmail.com>
2022-11-12 20:30:31 +1100

    update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('interactionCreate', async interaction => {
 
   if (interaction.commandName === 'ping') {
     const pingvalue = client.ws.ping;
-    const replyMsg = '🏓 Pong! Hosted on Heroku' + '/n' + 'Latency: ' + pingvalue;
+    const replyMsg = '🏓 Pong! Hosted on Heroku' + '\n' + 'Latency: ' + pingvalue + 'ms';
     await interaction.reply(replyMsg);
   }
 });
