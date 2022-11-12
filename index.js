1f6bba735915775fdc5490229534e4825a75220c
Mitwire <mitwire@gmail.com>
2022-11-12 20:42:53 +1100

    update

--- index.js
+++ index.js
@@ -38,6 +38,32 @@ client.on('interactionCreate', async interaction => {
     const replyMsg = 'Pong! Hosted on Heroku' + '\n' + 'Latency: ' + pingvalue + 'ms' + ' 🏓';
     await interaction.reply(replyMsg);
   }
+
+  if (interaction.commandName === 'embed') {
+   
+    const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields(
+      { name: 'Regular field title', value: 'Some value here' },
+      { name: '\u200B', value: '\u200B' },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+      { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+    //channel.send({ embeds: [exampleEmbed] });
+
+    await interaction.reply({ embeds: [exampleEmbed] });
+  }
+
 });
 
 
