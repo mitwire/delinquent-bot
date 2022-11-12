216613df057e6ce6a0f0356136930e700011edeb
Mitwire <mitwire@gmail.com>
2022-11-12 19:30:44 +1100

    update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
-    await interaction.reply('🏓 Pong!');
+    await interaction.reply('🏓 Pong!' + client.ws.ping);
   }
 });
 
