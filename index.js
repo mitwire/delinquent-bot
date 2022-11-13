9c105e1eb2270107b7136c708ff74d14ed8247b0
Mitwire <mitwire@gmail.com>
2022-11-13 12:14:12 +1100

    upd

--- index.js
+++ index.js
@@ -108,7 +108,6 @@ client.on('interactionCreate', async interaction => {
     const options = ["I honestly dont know","Probably","Probably not","Most likely","Theres no shot","Possibly","Yeah","Nope","Not at all","Definetly"];
     const result = options[Math.floor(Math.random() * options.length)]
     const exampleEmbed = new EmbedBuilder()
-
     .setColor(0xf1e65d)
     .setTitle(result)
 
